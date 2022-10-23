e740c1e909990b9bf4b15d8bd607a32fa77c6ae9
jjmchew <jjmchew@gmail.com>
2022-10-23 13:41:28 -0600

    updates based upon code review feedback

--- rb101/lesson_2/rpsls.rb
+++ rb101/lesson_2/rpsls.rb
@@ -1,52 +1,43 @@
 # rock paper scissors lizard Spock
 
 VALID_CHOICES = %w(rock paper scissors lizard spock)
+
 OPTIONS = %w(r p sc l sp)
 
-def match_outcome?(first, second)
-  outcome = {
-    'rock': {
-      paper: {win: 'paper', msg: 'paper covers rock'},
-      scissors: {win: 'rock', msg: 'rock crushes scissors'},
-      spock: {win: 'spock', msg: 'Spock vaporizes rock'},
-      lizard: {win: 'rock', msg: 'rock crushes lizard'}
-    },
-    'paper': {
-      scissors: {win: 'scissors', msg: 'scissors cut paper'},
-      spock: {win: 'paper', msg: 'paper disproves Spock'},
-      lizard: {win: 'lizard', msg: 'lizard eats paper'},
-      rock: {win: 'paper', msg: 'paper covers rock'}
-    },
-    'scissors': {
-      spock: {win: 'spock', msg: 'Spock smashes scissors'},
-      lizard: {win: 'scissors', msg: 'scissors decapitates lizard'},
-      rock: {win: 'rock', msg: 'rock crushes scissors'},
-      paper: {win: 'scissors', msg: 'scissors cut paper'},
-    },
-    'spock': {
-      lizard: {win: 'lizard', msg: 'lizard poisons Spock'},
-      rock: {win: 'spock', msg: 'Spock vaporizes rock'},
-      paper: {win: 'paper', msg: 'paper disproves Spock'},
-      scissors: {win: 'spock', msg: 'Spock smashes scissors'},
-    },
-    'lizard': {
-      rock: {win: 'rock', msg: 'rock crushes lizard'},
-      paper: {win: 'lizard', msg: 'lizard eats paper'},
-      scissors: {win: 'scissors', msg: 'scissors decapitates lizard'},
-      spock: {win: 'lizard', msg: 'lizard poisons Spock'},
-    }
-  }
+WINS_AGAINST = {
+  'rock' => ['scissors', 'lizard'],
+  'paper' => ['rock', 'spock'],
+  'scissors' => ['paper', 'lizard'],
+  'lizard' => ['spock', 'paper'],
+  'spock' => ['scissors', 'rock']
+}
 
-  return outcome[first.to_sym][second.to_sym]
+OUTCOME_MESSAGES = {
+  ['paper', 'rock'] => 'paper covers rock',
+  ['rock', 'scissors'] => 'rock crushes scissors',
+  ['rock', 'spock'] => 'Spock vaporizes rock',
+  ['lizard', 'rock'] => 'rock crushes lizard',
+  ['paper', 'scissors'] => 'scissors cut paper',
+  ['paper', 'spock'] => 'paper disproves Spock',
+  ['lizard', 'paper'] => 'lizard eats paper',
+  ['scissors', 'spock'] => 'Spock smashes scissors',
+  ['lizard', 'scissors'] => 'scissors decapitates lizard',
+  ['lizard', 'spock'] => 'lizard poisons Spock'
+}
+
+def display_blank_line
+  puts
 end
 
-def get_result(player_choice, computer_choice)
+def get_match_outcome(player_choice, computer_choice)
   if player_choice == computer_choice
-    {winner: 'tie', msg: ''}
-  elsif match_outcome?(player_choice, computer_choice)[:win] == player_choice
-    {winner: 'player', msg: match_outcome?(player_choice, computer_choice)[:msg]}
-  elsif match_outcome?(player_choice, computer_choice)[:win] == computer_choice
-    {winner: 'computer', msg: match_outcome?(player_choice, computer_choice)[:msg]}
+    { winner: 'tie', msg: '' }
+  elsif WINS_AGAINST[player_choice].include?(computer_choice)
+    { winner: 'player',
+      msg: OUTCOME_MESSAGES[[player_choice, computer_choice].sort] }
+  else
+    { winner: 'computer',
+      msg: OUTCOME_MESSAGES[[player_choice, computer_choice].sort] }
   end
 end
 
@@ -64,29 +55,31 @@ def return_full_choice(choice)
   end
 end
 
-def get_player_choice
-
-  choice = ''
+def display_choose_message
   prompt "Choose one:   rock   paper  scissors  lizard  Spock"
   puts "            type:   r      p      sc        l       sp"
-  puts
+  display_blank_line
+end
 
-  loop do 
+def display_invalid_message
+  display_blank_line
+  prompt "That's not a valid choice."
+  display_blank_line
+end
+
+def get_player_choice
+  choice = ''
+  display_choose_message
+  loop do
     print '                  > '
-    
     choice = gets.chomp.strip.downcase
 
-    if (OPTIONS+VALID_CHOICES).include? choice
-      break
-    else
-      puts
-      prompt "That's not a valid choice."
-      puts
+    if (OPTIONS + VALID_CHOICES).include? choice then break
+    else display_invalid_message
     end
   end
 
   return_full_choice(choice)
-
 end
 
 def increment_score(winner, score)
@@ -113,55 +106,55 @@ def reset_choice(choice)
 end
 
 def display_title
-  puts
-  puts '  +--------------------------------------------------------------------------+ '
-  puts '  |    R O C K    P A P E R    S C I S S O R S    S P O C K    L I Z A R D   | '
-  puts '  +--------------------------------------------------------------------------+ '
+  display_blank_line
+  puts '  +--------------------------------------------------------------------------+'
+  puts '  |    R O C K    P A P E R    S C I S S O R S    S P O C K    L I Z A R D   |'
+  puts '  +--------------------------------------------------------------------------+'
 end
 
-def draw_layout(score, choice, result)
+def draw_layout(score, choice)
   system('clear') || system('cls')
-
   display_title
-  
-  puts
+  display_blank_line
   puts '                    +-----------------+-----------------+'
   puts "                    |       You       |    Computer     |"
   puts '                    +-----------------+-----------------+'
   puts "             WINS   |        #{score[:player]}        |        #{score[:computer]}        |"
-  puts '                    +-----------------+-----------------+'                                     
+  puts '                    +-----------------+-----------------+'
   puts "           CHOICE   |#{display_choice choice[:player]}|#{display_choice choice[:computer]}|"
-  puts '                    +-----------------+-----------------+'                                     
-  puts
+  puts '                    +-----------------+-----------------+'
+  display_blank_line
 end
 
-def display_outcome(result)
-  if result[:winner] == 'player'
-    puts "                       ... #{result[:msg]}"
-    puts
-    puts
-    puts '                            ****  YOU WIN !!  ****'
-    puts
-  elsif result[:winner] == 'computer'
-    puts "                       ... #{result[:msg]}"
-    puts
-    puts
-    puts '                                 You LOST !!'
-    puts
-  elsif result[:winner] == 'tie'
-    puts
-    puts '                                 You TIED !!'
-    puts
+def display_tie_message
+  display_blank_line
+  puts "                                 You TIED !!"
+  display_blank_line
+end
+
+def display_outcome_message(message1, message2)
+  puts "                       ... #{message1}"
+  display_blank_line
+  display_blank_line
+  puts "                            #{message2}"
+  display_blank_line
+end
+
+def display_outcome(match_outcome)
+  if match_outcome[:winner] == 'tie' then display_tie_message
+  elsif match_outcome[:winner] == 'player'
+    display_outcome_message match_outcome[:msg], '****  YOU WIN !!  ****'
+  elsif match_outcome[:winner] == 'computer'
+    display_outcome_message match_outcome[:msg], '     You LOST !!'
   end
 end
 
 def get_pause(score)
-  unless score[:player] == 3 || score[:computer] == 3
-    puts
-    puts '                    press any key to continue the battle'
-  else
-    puts
-    puts '                         press any key to continue'
+  display_blank_line
+  if score[:player] != 3 && score[:computer] != 3
+    puts '                           press enter to continue'
+  elsif score[:player] == 3 || score[:computer] == 3
+    puts '                     press enter to continue the battle'
   end
   gets.chomp! # pause
 end
@@ -171,16 +164,16 @@ def display_instructions
 
   display_title
 
-  puts
-  puts
+  display_blank_line
+  display_blank_line
   puts "            This is an updated version of Rock, Paper, Scissors."
   puts "                  Give it a go, and you'll figure it out."
-  puts
+  display_blank_line
   puts "                    Winner is the first to win 3 games."
-  puts
-  puts
-  puts
-  puts '                         press any key to continue'
+  display_blank_line
+  display_blank_line
+  display_blank_line
+  puts '                         press enter to continue'
   gets.chomp! # pause
 end
 
@@ -191,55 +184,54 @@ def display_final_message(score)
   else
     puts '                    Tough break - better luck next time!'
   end
-  puts
-  puts
+  display_blank_line
+  display_blank_line
 end
 
 def get_play_again
   prompt "Want to try again?   y to play, any key to exit"
-  puts
+  display_blank_line
   print '                         > '
-  answer = gets.chomp.strip
+  gets.chomp.strip
 end
 
 # START
-
 display_instructions
 
 loop do
-
   # initialize game variables
-  score = {player: 0, computer: 0}
-  choice = {player: '', computer: ''}
-  result = {winner: '', msg: ''}
+  score = { player: 0, computer: 0 }
+  choice = { player: '', computer: '' }
+  match_outcome = { winner: '', msg: '' }
 
   loop do
     # re-draw screen
-    draw_layout score, choice, result
+    draw_layout score, choice
 
     # get choices
     choice[:player] = get_player_choice
     choice[:computer] = VALID_CHOICES.sample
 
     # display outcomes
-    draw_layout score, choice, result
-    
-    result = get_result choice[:player], choice[:computer]
-    increment_score(result[:winner], score) unless result[:winner] == 'tie'
-    
-    display_outcome result
+    draw_layout score, choice
+
+    match_outcome = get_match_outcome choice[:player], choice[:computer]
+    if match_outcome[:winner] != 'tie'
+      increment_score match_outcome[:winner], score
+    end
+
+    display_outcome match_outcome
 
     # pause
     get_pause score
     reset_choice choice
 
-    break if score[:player] == 3 || score[:computer] == 3 
+    break if score[:player] == 3 || score[:computer] == 3
   end
 
-  draw_layout(score, choice, result)
+  draw_layout score, choice
   display_final_message score
- 
+
   play_again = get_play_again
   break unless play_again.downcase.start_with? 'y'
-
 end
