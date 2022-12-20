49a59f9e51679cd27c5e1083c14564a61c34ed23
jjmchew <jjmchew@gmail.com>
2022-12-20 15:12:35 -0700

    updates

---
+++ rb120/debugging/07.rb
@@ -0,0 +1,61 @@
+class BankAccount
+  attr_reader :balance
+
+  def initialize(account_number, client)
+    @account_number = account_number
+    @client = client
+    @balance = 0
+  end
+
+  def deposit(amount)
+    if amount > 0
+      self.balance += amount
+      "$#{amount} deposited. Total balance is $#{balance}."
+    else
+      "Invalid. Enter a positive amount."
+    end
+  end
+
+  def withdraw(amount)
+    if amount > 0 && valid_transaction?(balance - amount)  # updated
+      self.balance -= amount                               # line updated - previously was not a conditional, but an assignment statement
+      success = true                                       # updated
+    else
+      success = false
+    end
+
+    if success
+      "$#{amount} withdrawn. Total balance is $#{balance}."
+    else
+      "Invalid. Enter positive amount less than or equal to current balance ($#{balance})."
+    end
+  end
+
+  def balance=(new_balance)
+    if valid_transaction?(new_balance)
+      @balance = new_balance
+      true
+    else
+      false
+    end
+    # @balance = new_balance
+  end
+
+  def valid_transaction?(new_balance)
+    new_balance >= 0
+  end
+end
+
+# Example
+
+account = BankAccount.new('5538898', 'Genevieve')
+
+                          # Expected output:
+p account.balance         # => 0
+p account.deposit(50)     # => $50 deposited. Total balance is $50.
+p account.balance         # => 50
+p account.withdraw(80)    # => Invalid. Enter positive amount less than or equal to current balance ($50).
+                          # Actual output: $80 withdrawn. Total balance is $50.
+p account.balance         # => 50
+p account.balance = -120
+p account.balance
