37f9a64b36bc3e9c554a5673348bef53575560a9
jjmchew <jjmchew@gmail.com>
2023-01-18 17:55:01 -0700

    updates

--- rb120/scratch.rb
+++ rb120/scratch.rb
@@ -1,18 +1,23 @@
-WHEELS = 6
-
-class Vehicle
-  # WHEELS = 4
-end
-
-# WHEELS = 8
-
-class Car < Vehicle
-  # WHEELS = 2
-
-  def wheels
-    WHEELS
+module Describable
+  def describe_shape
+    "I am a #{self.class} and have #{SIDES} sides."
   end
 end
 
-car = Car.new
-puts car.wheels        # => 4
+class Shape
+  include Describable
+
+  def self.sides
+    self::SIDES
+  end
+end
+
+class Quadrilateral < Shape
+  SIDES = 4
+end
+
+class Square < Quadrilateral; end
+
+Square.sides # => 4
+Square.new.sides # => 4
+Square.new.describe_shape # => "I am a Square and have 4 sides."
