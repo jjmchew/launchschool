189be9f81fa89671f07b400b3f4544fe9e67512a
jjmchew <jjmchew@gmail.com>
2023-07-03 21:47:35 -0600

    updates

---
+++ rb185/l1/expense/schema.sql
@@ -0,0 +1,18 @@
+--CREATE TABLE expenses (
+--  id serial PRIMARY KEY,
+--  amount numeric(6,2) NOT NULL,
+--  memo text NOT NULL,
+--  created_on date NOT NULL
+--);
+
+--ALTER TABLE expenses ADD CHECK (amount > 0);
+
+INSERT INTO expenses (amount, memo, created_on) VALUES (14.56, 'Pencils', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (3.29, 'Coffee', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES (49.99, 'Text Editor', NOW());
+INSERT INTO expenses (amount, memo, created_on) VALUES
+  (29.99, 'USB memory stick', NOW()),
+  (13.49, 'Ice cream', NOW()),
+  (160.45, 'Lift ticket', NOW()),
+  (24.32, 'Climbing gym day pass', NOW());
+
