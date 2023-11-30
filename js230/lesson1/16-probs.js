4f936a04f7f654eba9dcedebbdc737cc1f6bf166
jjmchew <jjmchew@gmail.com>
2023-11-29 22:34:11 -0700

    updates

--- js230/lesson1/16-probs.js
+++ js230/lesson1/16-probs.js
@@ -8,10 +8,27 @@ let ul = document.querySelector('#list');
 ul.classList.add('bulleted');
 
 // Q3
+function clickHandler(e) {
+  e.preventDefault();
+  let notice = document.querySelector('#notice');
+  if (notice.getAttribute('class') === 'hidden') {
+      notice.setAttribute('class', 'visible');
+  }
+  else notice.setAttribute('class', 'hidden');
+}
+
+let toggle = document.querySelector('#toggle');
+toggle.onclick = clickHandler;
+
 
 // Q4
+// continued from Q3
+document.querySelector('#notice').onclick = clickHandler;
 
 // Q5
+let multiplication = document.querySelector('#multiplication');
+multiplication.textContent = '117';
 
 // Q6
+document.body.setAttribute("id", 'styled');
 
